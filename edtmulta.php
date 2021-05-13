ecdf7f80ee13df32d4e69da8750e95ab67013d6f
Fabio Henrique Silva Furtado <fhs.furtado@gmail.com>
2021-05-13 16:47:48 -0300

    adjusting log acts

--- edtmulta.php
+++ edtmulta.php
@@ -27,6 +27,29 @@
         } catch(PDOException $e){
             echo $e->getMessage();
         }
+        // Dedo Duro Mode: ON
+        try{
+            $user_agents = array("iPhone","iPad","Android","webOS","BlackBerry","iPod","Symbian","IsGeneric");
+            $model = "Desktop";
+            foreach($user_agents as $user_agent){
+                if (strpos($_SERVER['HTTP_USER_AGENT'], $user_agent) !== FALSE) {
+                    $model	= $user_agent;
+                    break;
+                }
+            }
+            $meuip = json_decode(file_get_contents('https://api.ipify.org?format=json'),true);
+            $save = $bd->prepare('INSERT INTO tb_log (id_user,uf,cidade,ip,dispositivo,data_login,acao) VALUES (:user,:uf,:cidade,:ip,:dispositivo,NOW(),:acao)');
+            $save->bindParam(':user',$user);
+            $save->bindParam(':uf',$myuf);
+            $save->bindParam(':cidade',$mycidade);
+            $save->bindParam(':dispositivo',$model);
+            $save->bindParam(':ip',$meuip['ip']);
+            $acao = 'Editou multa de ID '.$id;
+            $save->bindParam(':acao',$acao);
+            $save->execute();
+        } catch(PDOException $e){
+            echo $e->getMessage();
+        }
         header('Location: multas.php?edt=1');
     } else{
         echo "Ops! Erro não identificado";
